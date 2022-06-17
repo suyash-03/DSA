734d330fde294170a616d492544489125d80b708
suyash-03 <suyash.singh9450@gmail.com>
2022-06-17 23:52:03 +0530

    Unordered_map STL

---
+++ Map/Unordered Map/unordered_map.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<unordered_map>
+
+using namespace std;
+
+int main(){
+    unordered_map<string,int> myMap;
+    
+    //Method 1 to Insert
+    pair<string,int> p("Suyash",100);
+    myMap.insert(p);
+
+    //Method 2 to Insert
+    myMap["Shreyas"] = 99;
+
+    //Method 1 to Print
+    cout<<myMap["Suyash"]<<endl;
+
+    //Method 2 to Print
+    cout<<myMap.at("Shreyas")<<endl;
+
+    /* If a key does not exist and we access it
+       via this method default value 0 gets printed
+       However cout<<myMap.at("Arthur"); will throw an error  */
+    
+    cout<<myMap["Arthur"]<<endl;
+    cout<<myMap.size()<<endl;
+
+    //To check presence of a key
+    if(myMap.count("John") == 0){
+        cout<<"Key John doest not exist";
+    }
+}
