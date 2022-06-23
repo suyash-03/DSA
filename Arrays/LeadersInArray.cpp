bedab0744d2716ab5e345381f2d7a1f2a8ad0317
suyash-03 <suyash.singh9450@gmail.com>
2022-06-23 13:38:16 +0530

    Leaders Change

--- Arrays/LeadersInArray.cpp
+++ Arrays/LeadersInArray.cpp
@@ -4,23 +4,19 @@
 using namespace std;
 
 vector<int> leaders(vector<int> A){
-    vector<int> result;
-     for (int i = 0; i < A.size(); i++)
-    {
-        int j;
-        for (j = i+1; j < A.size(); j++)
-        {
-            if (A[i] < A[j])
-                {
-                 break;   
-                }
-        }   
-        if (j == A.size()){
-           result.push_back(A[i]); 
-        } // the loop didn't break
-            
-  }
-  return result;
+     vector<int> result;
+    //Traverse the array from right to left;
+    int length = A.size();
+    int maxFromRight = A[length-1];
+    result.push_back(A[length - 1]);
+    
+    for(int i = length-2; i>=0 ; i--){
+        if(A[i] > maxFromRight){
+            result.push_back(A[i]);
+            maxFromRight = A[i];
+        }
+    }
+    return result;
 }
 
 int main(){
