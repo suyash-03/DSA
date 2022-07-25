f9cdc234bf2d0292b3b1f1d003696af9f615a8b2
Suyash Singh <76098221+suyash-03@users.noreply.github.com>
2022-07-25 16:47:42 +0530

    Update CoinChange2.cpp

--- Dynamic Programming/CoinChange2.cpp
+++ Dynamic Programming/CoinChange2.cpp
@@ -6,33 +6,26 @@ using namespace std;
 
 class Solution {
 public:
-    int dfs(int n,vector<int> &coins,int amount,vector<vector<int>> &dp){
-        if(amount == 0){
+    int dfs(int n, vector<int> &coins,int amount,vector<vector<int>> &dp){
+        if(n <= 0 && amount != 0){
             return dp[n][amount] = 0;
         }
-        if( n == 0 && amount != 0){
-            return dp[n][amount] = INT_MAX;
+        if(amount == 0){
+            return dp[n][amount] = 1;
         }
         if(dp[n][amount] != -1){
             return dp[n][amount];
         }
-        
         if(coins[n-1] <= amount){
-            return dp[n][amount] = 
-                min( 1ll + dfs(n,coins,amount - coins[n-1],dp) , dfs(n-1,coins,amount,dp) + 0ll );
+            return dp[n][amount] = (dfs(n,coins,amount-coins[n-1],dp)+dfs(n-1,coins,amount,dp));
+        }else{
+            return dp[n][amount] = dfs(n-1,coins,amount,dp);
         }
-        else return dp[n][amount] = dfs(n-1,coins,amount,dp);
     }
-
-    int coinChange(vector<int>& coins, int amount) {
+    int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>> dp(n+1,vector<int>(amount+1,-1));
-        int res =  dfs(n,coins,amount,dp);
-        if(res == INT_MAX){
-            return -1;
-        }else{
-            return res;
-        }
+        return dfs(n,coins,amount,dp);
     }
 };
 
