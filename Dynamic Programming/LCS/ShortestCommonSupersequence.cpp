31e99d9039b9526d2a6590424bdbc23c9875acbc
suyash-03 <suyash.singh9450@gmail.com>
2022-07-28 16:49:19 +0530

    ShortestCommonSuperSequence

--- Dynamic Programming/LCS/ShortestCommonSupersequence.cpp
+++ Dynamic Programming/LCS/ShortestCommonSupersequence.cpp
@@ -2,9 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-public:
-    //Make Sure to pass the strings by reference
+class Solution
+{
+    public:
+    //Function to find length of shortest common supersequence of two strings.
     int longestCommonSubsequenceUtil(string &text1,string &text2,int n,int m,vector<vector<int>> &dp){
         if(n == 0 || m == 0){
             return 0;
@@ -19,20 +20,30 @@ public:
                 max(longestCommonSubsequenceUtil(text1,text2,n-1,m,dp),longestCommonSubsequenceUtil(text1,text2,n,m-1,dp));
         }
     }
+
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
         return longestCommonSubsequenceUtil(text1,text2,n,m,dp);
     }
+
+    
+    int shortestCommonSupersequence(string X, string Y, int m, int n)
+    {
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        string temp = X + Y;
+        int lengthOfLCS = longestCommonSubsequence(X,Y);
+        cout<<"Length of Longest Common Subsequence is: "<<lengthOfLCS<<endl;
+        return (temp.size() - lengthOfLCS);
+    }
 };
 
 int main(){
     Solution s;
-    string s1 = "AVnQIELxFb";
-    string s2 = "TEpRbSvi";
-    int res = s.longestCommonSubsequence(s1,s2);
-    cout<<res<<endl;
-
+    string X = "AVnQIELxFb"; int n = X.size();
+    string Y = "TEpRbSvi"; int m = Y.size();
+    int res = s.shortestCommonSupersequence(X,Y,n,m);
+    cout<<"Length of Shortest Common Supersequence is: "<<res<<endl;
     return 0;
 }
