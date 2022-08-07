f97f2807846142f14a54b33dcbb60de2e5c4bb3f
suyash-03 <suyash.singh9450@gmail.com>
2022-08-08 00:11:31 +0530

    Change

--- Dynamic Programming/LongestPalindromicSubstringLength.cpp
+++ Dynamic Programming/LongestPalindromicSubstringLength.cpp
@@ -1,35 +1,36 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-
-int util(int i,int j,string s,vector<vector<int>> &dp){
-    if(i>j){
-        return 0;
-    }
-    if(i == j){
-        return 1;
-    }
-    if(dp[i][j] != -1){
-        return dp[i][j];
-    }
-    
-    if(s[i] == s[j]){
-        int lengthRemaining = j-i-1;
-        if(lengthRemaining == util(i+1,j-1,s,dp)){
-            return dp[i][j] = 2+lengthRemaining;
+class Solution{
+    public:
+    int util(int i,int j,string s,vector<vector<int>> &dp){
+        if(i>j){
+            return 0;
         }
+        if(i == j){
+            return 1;
+        }
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        if(s[i] == s[j]){
+            int lengthRemaining = j-i-1;
+            if(lengthRemaining == util(i+1,j-1,s,dp)){
+                return dp[i][j] = 2+lengthRemaining;
+            }
+        }
+        return dp[i][j] = max(util(i+1,j,s,dp),util(i,j-1,s,dp));
     }
-    return dp[i][j] = max(util(i+1,j,s,dp),util(i,j-1,s,dp));
-}
-
-int longestPalindromicSubstring(string s){
-    int n = s.size();
-    vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-    return util(0,s.size()-1,s,dp);
-}
+    int longestPalindromicSubstring(string s){
+        int n = s.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return util(0,s.size()-1,s,dp);
+    }
+};
 
 int main(){
+    Solution sol;
     string s = "babad";
-    int res = longestPalindromicSubstring(s);
+    int res = sol.longestPalindromicSubstring(s);
     cout<<res;
 }
