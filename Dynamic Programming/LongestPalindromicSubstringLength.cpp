005e9493c5dc306ee26d678339c5c943d2350858
suyash-03 <suyash.singh9450@gmail.com>
2022-08-07 23:59:40 +0530

    D

--- Dynamic Programming/LongestPalindromicSubstringLength.cpp
+++ Dynamic Programming/LongestPalindromicSubstringLength.cpp
@@ -9,6 +9,10 @@ int util(int i,int j,string s,vector<vector<int>> &dp){
     if(i == j){
         return 1;
     }
+    if(dp[i][j] != -1){
+        return dp[i][j];
+    }
+    
     if(s[i] == s[j]){
         int lengthRemaining = j-i-1;
         if(lengthRemaining == util(i+1,j-1,s,dp)){
